52cd73f80dd383cbcc45277109dcb71061736f3b
Ingo Wolf <me@ingowolf.au>
2025-06-30 13:33:17 +1000

    add es

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,61 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	let es = [
+		'E',
+		'Ē',
+		'É',
+		'Ě',
+		'È',
+		'Ê',
+		'Ę',
+		'Ë',
+		'Ė',
+		'Ẹ',
+		'Ẽ',
+		'Ḗ',
+		'Ể',
+		'Ẻ',
+		'Ệ',
+		'Ế',
+		'Ễ',
+		'Є',
+		'Ё̄',
+		'Ҽ',
+		'ⱻ',
+		'€',
+		'℮',
+		'ⅇ',
+		'𝔼',
+		'∈',
+		'ⓔ',
+		'📧',
+		'🅴',
+		'🄴'
+	];
+</script>
+
 <div class="flex h-screen w-screen flex-col items-center justify-center">
 	<div class="grid h-fit w-fit grid-cols-5 gap-2">
 		{#each Array(30)}
 			<div class="size-15 rounded border"></div>
+		{/each}
+	</div>
+	<div class="flex flex-row">
+		{#each es.slice(0, 11) as key}
+			<Button>{key}</Button>
+		{/each}
+	</div>
+
+	<div class="flex flex-row">
+		{#each es.slice(11, 21) as key}
+			<Button>{key}</Button>
+		{/each}
+	</div>
+
+	<div class="flex flex-row">
+		{#each es.slice(21, es.length) as key}
+			<Button>{key}</Button>
 		{/each}
 	</div>
 </div>
