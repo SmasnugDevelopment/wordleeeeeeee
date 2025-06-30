bb2d2632e1816613248ec3cbaabc3eaba5dd3fc7
Ingo Wolf <me@ingowolf.au>
2025-06-30 13:21:24 +1000

    center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<div class="grid h-fit w-fit grid-cols-5 gap-2">
-	{#each Array(30)}
-		<div class="size-15 rounded border"></div>
-	{/each}
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<div class="grid h-fit w-fit grid-cols-5 gap-2">
+		{#each Array(30)}
+			<div class="size-15 rounded border"></div>
+		{/each}
+	</div>
 </div>
